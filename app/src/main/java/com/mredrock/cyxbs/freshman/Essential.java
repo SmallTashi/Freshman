ce431a660e9a2bf03acd0fe2d4cd7f546f7798b8
thashi <xzmaoao123@outlook.com>
2018-08-08 23:28:06 +0800

    Initial commit

---
+++ app/src/main/java/com/mredrock/cyxbs/freshman/Essential.java
@@ -0,0 +1,13 @@
+package com.mredrock.cyxbs.freshman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Essential extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.freshman_layout_essential);
+    }
+}
