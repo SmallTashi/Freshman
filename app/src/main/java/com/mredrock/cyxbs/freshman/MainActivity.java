f06f80920cb61ac954f18fe0ee6ec99c67188194
thashi <xzmaoao123@outlook.com>
2018-08-09 00:02:34 +0800

    init

--- app/src/main/java/com/mredrock/cyxbs/freshman/MainActivity.java
+++ app/src/main/java/com/mredrock/cyxbs/freshman/MainActivity.java
@@ -3,11 +3,11 @@ package com.mredrock.cyxbs.freshman;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Essential extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.freshman_layout_essential);
+        setContentView(R.layout.freshman_activity_main);
     }
 }
